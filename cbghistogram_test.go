d36f00c31300e77050b59dd7e0ed383060ceae94
abhinavdangeti <abhinav@couchbase.com>
2017-02-10 16:50:27 -0800

    Adding tests for cbghistogram

--- cbghistogram_test.go
+++ cbghistogram_test.go
@@ -15,29 +15,137 @@
 package cbghistogram
 
 import (
+	"math/rand"
 	"testing"
 )
 
 func TestNewHistogram(t *testing.T) {
+	tests := []struct {
+		name string
+		bins int
+	}{
+		{"TestNewHistogram_bins_1", 1},
+		{"TestNewHistogram_bins_10", 10},
+	}
 
+	for _, test := range tests {
+		hist := NewHistogram(test.name, test.bins)
+		if hist._name != test.name {
+			t.Errorf("Histogram name doesn not match!")
+		}
+
+		// Account for 2 additional bins: smallest and largest
+		// created by the Histogram
+		if len(hist._bins) != test.bins + 2 {
+			t.Errorf("Correct number of bins not created!")
+		}
+	}
 }
 
 func TestAdd(t *testing.T) {
+	tests := []struct {
+		name string
+		bins int
+		samples int
+		mod_factor uint64
+	}{
+		{"TestAdd_10_10000_10", 10, 10000, 10},
+		{"TestAdd_10_20000_5", 10, 20000, 5},
+	}
 
+	for _, test := range tests {
+		hist := NewHistogram(test.name, test.bins)
+		for i := 0; i < test.samples; i++ {
+			hist.Add(uint64(rand.Uint32()) % test.mod_factor, 1)
+		}
+
+		count := 0
+		for i := 0; i < len(hist._bins); i++ {
+			count += int(hist._bins[i]._count)
+		}
+
+		if count != test.samples {
+			t.Errorf("Incorrect number of samples in the histogram!")
+		}
+	}
 }
 
 func TestParallelAdd(t *testing.T) {
+	hist := NewHistogram("TestParallelAdd", 10)
 
+	ch := make(chan int)
+
+	sample_count_1 := 100000
+	sample_count_2 := 200000
+
+	go func() {
+		for i := 0; i < sample_count_1; i++ {
+			hist.Add(uint64(rand.Uint32()) % 10, 1)
+		}
+		ch <- 1
+	}()
+
+	go func() {
+		for i := 0; i < sample_count_2; i++ {
+			hist.Add(uint64(rand.Uint32()) % 10, 1)
+		}
+		ch <- 1
+	}()
+
+	<-ch
+	<-ch
+
+	count := 0
+	for i := 0; i < len(hist._bins); i++ {
+		count += int(hist._bins[i]._count)
+	}
+
+	if count != sample_count_1 + sample_count_2 {
+		t.Errorf("Incorrect number of samples in the histogram!")
+	}
 }
 
 func TestGraph(t *testing.T) {
+	hist := NewHistogram("TestGraph", 5)
 
+	for i := 0; i < 10000; i++ {
+		hist.Add(uint64(i) % 5, 1)
+	}
+
+	buf := hist.EmitGraph()
+
+	exp := `TestGraph (10000 Total)
+[0 - 1]       2000   20.00% ###############
+[1 - 2]       2000   20.00% ###############
+[2 - 4]       4000   40.00% ##############################
+[4 - 8]       2000   20.00% ###############
+`
+	got := buf.String()
+	if got != exp {
+		t.Errorf("Expected:\n%s\n, But got:\n%s", exp, got)
+	}
 }
 
-func BenchmarkAdd_100000(b *testing.B) {
+func BenchmarkAdd(b *testing.B) {
+	hist := NewHistogram("BenchmarkAdd", 100)
 
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		hist.Add(uint64(i), 1)
+	}
 }
 
-func BenchmarkGraph_100000(b *testing.B) {
+func BenchmarkGraph(b *testing.B) {
+	hist := NewHistogram("BenchMarkGraph", 100)
 
+	for i := 0; i < b.N; i++ {
+		hist.Add(uint64(i), 1)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		hist.EmitGraph()
+	}
 }
